e96ac75c90ad9f0446b8579eb33861c57bd0d357
anb05 <alexandr05@list.ru>
2017-12-25 20:36:23 +0200

    created Meow

---
+++ Models/Meow.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file contains a meow simulation class
+ *
+ * PHP version 7.2
+ *
+ * @category TestExercise
+ *
+ * @package ForWebbyLab
+ *
+ * @author anb05 <alexandr05@list.ru>
+ *
+ * @license MIT <http://opensource.org/licenses/MIT>
+ *
+ * @link https://github.com/anb05/testExerciseWebbylab
+ */
+
+declare(strict_types = 1);
+
+namespace Models;
+
+use Contracts\VoiceBehavior;
+
+/**
+ * This class implements a meow simulation
+ *
+ * @category TestExercise
+ *
+ * @package ForWebbyLab
+ *
+ * @author anb05 <alexandr05@list.ru>
+ *
+ * @license MIT <http://opensource.org/licenses/MIT>
+ *
+ * @link https://github.com/anb05/testExerciseWebbylab
+ */
+class Meow implements VoiceBehavior
+{
+    /**
+     * This method simulates mouw
+     *
+     * @return string
+     */
+    public function say()
+    {
+        return "mouw \n";
+    }
+}
+
